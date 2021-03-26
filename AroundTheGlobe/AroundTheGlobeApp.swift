c95e1cee250e0b6a0bc167faff982aa6be58ad5a
Navati Jain <>
2021-03-26 01:09:43 -0400

    Initial commit

---
+++ AroundTheGlobe/AroundTheGlobeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AroundTheGlobeApp.swift
+//  AroundTheGlobe
+//
+//  Created by Navati Jain on 2021-03-26.
+//
+
+import SwiftUI
+
+@main
+struct AroundTheGlobeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AroundTheGlobe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AroundTheGlobe
+//
+//  Created by Navati Jain on 2021-03-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
